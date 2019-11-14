236e64d5e6b2407505c6d48283a1c09d04a982ac
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-11-14 15:05:05 +0000

    making it say hello

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class HelloWorld extends React.Component {
+    render() {
+        return(
+            <div>Hello World</div>
+            )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDom.render(<HelloWorld />, document.getElementById('root'));
